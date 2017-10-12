f92649743a98c31694abef6df5374450faf112ab
Joshua Palacios <palacios.joshua@gmail.com>
2017-10-12 04:50:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,34 @@ function destructivelyAppendKitten(name) {
   Kitten.push(name)
   return Kitten
 }
+
+function destructivelyPrependKitten(name) {
+  Kitten.unshift(name)
+  return Kitten
+}
+
+function destructivelyRemoveLastKitten() {
+  Kitten.pop()
+  return Kitten
+}
+
+function destructivelyRemoveFirstKitten() {
+  Kitten.shift()
+  return Kitten
+}
+
+function appendKitten(name) {
+  return [...Kittens, name]
+}
+
+function prependKitten(name) {
+  return [name, ...Kittens]
+}
+
+function removeLastKitten() {
+  return Kitten.slice(0, Kitten.legnth - 1)
+}
+
+function removeFirstKitten() {
+  return Kitten.slice(1)
+}
